dc8f68bdfdfc61315357dc43510b3ec30bf00b0a
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-08 10:47:51 -0300

    Initial config

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # niri = {
+    #   url = "github:sodiboo/niri-flake";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
+    let 
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      lib = nixpkgs.lib;
+
+      mkSystem = pkgs: system: hostname:
+        pkgs.lib.nixosSystem {
+	  system = system;
+	  modules = [
+	    { networking.hostName = hostname; }
+	    ./modules/system/configuration.nix
+	    (./. + "/hosts/${hostname}/hardware-configuration.nix")
+	    home-manager.nixosModules.home-manager {
+	      home-manager = {
+	        useUserPackages = true;
+		useGlobalPkgs = true;
+		extraSpecialArgs = { inherit inputs; };
+		user.luiz = (./. + "/hosts/${hostname}/user.nix");
+	      };
+	    }
+	  ];
+          specialArgs = { inherit inputs; };
+	};
+  in {
+    nixosConfigurations =  {
+      vm = mkSystem inputs.nixpkgs "x86_64-linux" "vm";
+      laptop = mkSystem inputs.nixpkgs "x86_64-linux" "laptop";
+    };
+  };
+}
