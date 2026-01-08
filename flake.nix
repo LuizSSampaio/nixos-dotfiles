a5936c18f7e337ccdd1ef6b5496a19bd72f1ed3c
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-08 11:04:29 -0300

    refactor: fix format

--- flake.nix
+++ flake.nix
@@ -14,32 +14,32 @@
   };
 
   outputs = { nixpkgs, home-manager, ... } @ inputs:
-    let 
+    let
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
       lib = nixpkgs.lib;
 
       mkSystem = pkgs: system: hostname:
         pkgs.lib.nixosSystem {
-	  system = system;
-	  modules = [
-	    { networking.hostName = hostname; }
-	    ./modules/system/configuration.nix
-	    (./. + "/hosts/${hostname}/hardware-configuration.nix")
-	    home-manager.nixosModules.home-manager {
-	      home-manager = {
-	        useUserPackages = true;
-		useGlobalPkgs = true;
-		extraSpecialArgs = { inherit inputs; };
-		user.luiz = (./. + "/hosts/${hostname}/user.nix");
-	      };
-	    }
-	  ];
-          specialArgs = { inherit inputs; };
-	};
-  in {
-    nixosConfigurations =  {
-      vm = mkSystem inputs.nixpkgs "x86_64-linux" "vm";
-      laptop = mkSystem inputs.nixpkgs "x86_64-linux" "laptop";
+	      system = system;
+	      modules = [
+	        { networking.hostName = hostname; }
+	        ./modules/system/configuration.nix
+	        (./. + "/hosts/${hostname}/hardware-configuration.nix")
+	        home-manager.nixosModules.home-manager {
+	          home-manager = {
+	          useUserPackages = true;
+		        useGlobalPkgs = true;
+		        extraSpecialArgs = { inherit inputs; };
+		        user.luiz = (./. + "/hosts/${hostname}/user.nix");
+	          };
+	        }
+	      ];
+        specialArgs = { inherit inputs; };
+	    };
+    in {
+      nixosConfigurations =  {
+        vm = mkSystem inputs.nixpkgs "x86_64-linux" "vm";
+        laptop = mkSystem inputs.nixpkgs "x86_64-linux" "laptop";
+      };
     };
-  };
 }
