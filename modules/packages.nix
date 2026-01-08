16a528708620bade4de5cd0ee535b8f8527b4c75
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-08 13:52:00 -0300

    feat(packages): add a list of default packages to use

---
+++ modules/packages.nix
@@ -0,0 +1,38 @@
+{ pkgs, ...}: let
+  gui = with pkgs; [
+    _1password-gui
+    mission-center
+    nautilus
+    pinta
+    gnome-calculator
+    gnome-disk-utility
+    localsend
+    qbittorrent
+    mpv
+    heroic
+    obsidian
+    prism-launcher
+  ];
+  cli = with pkgs; [
+    _1password-cli
+    atuin
+    fastfetch
+    ffmpeg
+    ripgrep
+    bat
+    fd
+    fzf
+    zoxide
+    eza
+  ];
+  dev = with pkgs; [
+    cmake
+    gcc
+    gnumake
+    opencode
+  ];
+in {
+  home = {
+    packages = gui ++ cli ++ dev;
+  };
+}
