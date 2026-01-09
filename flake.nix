0f955e83c0b8d3e666612671d6017c6d0d80f13d
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-09 09:46:24 -0300

    fix: removed niri definition at flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, niri-flake, ... } @ inputs:
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
     let
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
       lib = nixpkgs.lib;
