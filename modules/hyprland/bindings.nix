295c4728c73c58c7081da76dfe4cde3c378f8a30
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-10 11:17:03 -0300

    fix(hyprland): remove bindings message

--- modules/hyprland/bindings.nix
+++ modules/hyprland/bindings.nix
@@ -11,41 +11,41 @@ in {
       bind = [
         "$mod, RETURN, exec, $terminal"
 
-        "$mod, W, Close Window, killactive"
+        "$mod, W, killactive"
 
         # Control Tiling
-        "$mod, P, Toggle window split, togglesplit"
-        "$mod, T, Toggle window floating/tiling, togglefloating"
-        "$mod, F, Full screen, fullscreen, 0"
-        "$mod CTRL, F, Tiled full screen, fullscreenstate, 0 2"
-        "$mod ALT, F, Full width, fullscreen, 1"
+        "$mod, P, togglesplit"
+        "$mod, T, togglefloating"
+        "$mod, F, fullscreen, 0"
+        "$mod CTRL, F, fullscreenstate, 0 2"
+        "$mod ALT, F, fullscreen, 1"
 
         # Move Focus
-        "$mod, H, Move window focus left, movefocus, l"
-        "$mod, J, Move window focus down, movefocus, d"
-        "$mod, K, Move window focus up, movefocus, u"
-        "$mod, L, Move window focus right, movefocus, r"
+        "$mod, H, movefocus, l"
+        "$mod, J, movefocus, d"
+        "$mod, K, movefocus, u"
+        "$mod, L, movefocus, r"
 
         # Swap window position
-        "$mod SHIFT, H, Swap window to the left, swapwindow, l"
-        "$mod SHIFT, J, Swap window to the down, swapwindow, d"
-        "$mod SHIFT, K, Swap window to the up, swapwindow, u"
-        "$mod SHIFT, L, Swap window to the right, swapwindow, r"
+        "$mod SHIFT, H, swapwindow, l"
+        "$mod SHIFT, J, swapwindow, d"
+        "$mod SHIFT, K, swapwindow, u"
+        "$mod SHIFT, L, swapwindow, r"
 
         # Resize window
-        "$mod ALT, H, Resize window to the left, resizeactive, -100.0"
-        "$mod ALT, J, Resize window to the down, resizeactive, 100.0"
-        "$mod ALT, K, Resize window to the up, resizeactive, 0 -100.0"
-        "$mod ALT, L, Resize window to the right, resizeactive, 0 100.0"
+        "$mod ALT, H, resizeactive, -100.0"
+        "$mod ALT, J, resizeactive, 100.0"
+        "$mod ALT, K, resizeactive, 0 -100.0"
+        "$mod ALT, L, resizeactive, 0 100.0"
 
         # Move and resize with mouse
-        "$mod, mouse:272, Move window, movewindow"
-        "$mod, mouse:273, Resize window, resizewindow"
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
 
         # Clipboard
-        "$mod, C, Copy to clipboard, sendshortcut, CTRL, Insert"
-        "$mod, V, Paste from clipboard, sendshortcut, SHIFT, Insert"
-        "$mod, X, Cut to clipboard, sendshortcut, CTRL, X"
+        "$mod, C, sendshortcut, CTRL, Insert"
+        "$mod, V, sendshortcut, SHIFT, Insert"
+        "$mod, X, sendshortcut, CTRL, X"
 
         # TODO: Media Keys
         # TODO: Groups
