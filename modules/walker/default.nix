6473a7c776dc7b89350607488bba4f0fe2a79403
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-10 20:18:42 -0300

    fix(walker): another module fix

--- modules/walker/default.nix
+++ modules/walker/default.nix
@@ -8,12 +8,38 @@ in {
     enable = mkEnableOption "walker launcher";
   };
 
-  imports = [inputs.walker.homeManagerModules.default];
-
   config = mkIf cfg.enable {
+    imports = [ inputs.walker.homeManagerModules.default ];
+
     programs.walker = {
       enable = true;
       runAsService = true;
+
+      # Configuration options
+      config = {
+        theme = "default";
+        placeholders.default = {
+          input = "Search";
+          list = "No Results";
+        };
+        providers.prefixes = [
+          { provider = "websearch"; prefix = "+"; }
+          { provider = "providerlist"; prefix = "_"; }
+        ];
+        keybinds.quick_activate = ["F1" "F2" "F3"];
+      };
+
+      # Custom theme
+      themes = {
+        "my-theme" = {
+          style = ''
+            /* Your CSS here */
+          '';
+          layouts = {
+            "layout" = ''<!-- Your XML layout -->'';
+          };
+        };
+      };
     };
   };
 }
