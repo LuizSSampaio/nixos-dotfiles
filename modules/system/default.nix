11d3380fc88c76e9579b0ebe97f304b479aeb80c
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-10 20:20:15 -0300

    feat: add walker flake cache

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -54,6 +54,16 @@
       keep-outputs = true
       keep-derivations = true
     '';
+
+    extra-substituters = [
+      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+   ];
   };
 
   networking = {
