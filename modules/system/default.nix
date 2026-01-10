9d598bb7fe16bd43933092419ebe4d49bfdd457b
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-10 15:08:12 -0300

    feat(system): add auto update and garbage collection

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -6,4 +6,27 @@
     ./fonts.nix
     ./greetd.nix
   ];
+
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+    dates = "2:00";
+  };
+
+  nix = {
+    settings.auto-optimise-store = true;
+    settings.allowed-users = [ "luiz" ];
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '';
+  };
 }
