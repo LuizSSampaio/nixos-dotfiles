230ce27432ffe1b8b32642d5cc755e779cbca234
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-11 17:18:20 -0300

    feat(hyprland): remove hyprlock conflicts

--- modules/hyprland/hyprlock.nix
+++ modules/hyprland/hyprlock.nix
@@ -21,17 +21,9 @@ in {
           halign = "center";
           valign = "center";
 
-          inner_color = "#908000";
-          outer_color = "#d3c6aa";
           outline_thickness = 4;
 
-          font_family = "JetBrainsMono Nerd Font";
-          font_size = 32;
-          font_color = "#d3c6aa";
-
-          placeholder_color = "#d3c6aa";
           placeholder_text = "  Enter Password 󰈷 ";
-          check_color = "rgba(131, 192, 146, 1.0)";
           fail_text = "Wrong";
 
           rounding = 0;
@@ -43,9 +35,6 @@ in {
           monitor = "";
           text = "$FPRINTPROMPT";
           text_align = "center";
-          color = "rgb(211, 198, 170)";
-          font_size = 24;
-          font_family = "JetBrainsMono Nerd Font";
           position = "0, -100";
           halign = "center";
           valign = "center";
