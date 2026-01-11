aece2b30e682967b6106069ee4d48a59dae3efbc
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-11 15:12:07 -0300

    revert(git): revert eval warning fix

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -8,10 +8,8 @@ in {
   config = mkIf cfg.enable {
     programs.git = {
       enable = true;
-      user = {
-        name = "Luiz Henrique Silva Sampaio";
-        email = "luizhsampaio07@gmail.com";
-      };
+      userName = "Luiz Henrique Silva Sampaio";
+      userEmail = "luizhsampaio07@gmail.com";
     };
   };
 }
