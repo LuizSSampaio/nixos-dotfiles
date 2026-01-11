b4700d6f48a33b3979dbd89c1bc1763693764fad
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-11 13:43:10 -0300

    fix(hyprland): removed auto start since dunst service start on boot

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -3,14 +3,8 @@
 with lib;
 let cfg = config.modules.hyprland;
 in {
-  imports = [
-    ./autostart.nix
-    ./bindings.nix
-    ./env.nix
-    ./input.nix
-    ./looknfeel.nix
-    ./windows.nix
-  ];
+  imports =
+    [ ./bindings.nix ./env.nix ./input.nix ./looknfeel.nix ./windows.nix ];
 
   options.modules.hyprland = {
     enable = mkEnableOption "hyprland window manager";
