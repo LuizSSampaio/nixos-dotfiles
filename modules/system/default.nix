639742f67eec1d85ede5745b025bd78db0d0a00a
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-12 19:00:26 -0300

    feat(system): add locale settings

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -3,11 +3,6 @@
 {
   imports = [ ./greetd.nix ./nvidia.nix ./steam.nix ./plymouth.nix ];
 
-  # Bootloader configuration moved to host-specific files (hosts/*/system.nix)
-  # This allows different bootloader setups for VM (GRUB) vs laptop (systemd-boot + LUKS)
-
-  environment.systemPackages = with pkgs; [ neovim ];
-
   services.xserver.xkb = {
     layout = "us";
     variant = "intl";
@@ -16,6 +11,20 @@
   console.keyMap = "us-acentos";
 
   time.timeZone = "America/Sao_Paulo";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
 
   users.users.luiz = {
     isNormalUser = true;
