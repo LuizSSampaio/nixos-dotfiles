af85d03dea118f968765c84536debab17d3a60c4
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-12 14:23:15 -0300

    feat(hyprland): add more bindings

--- modules/hyprland/bindings.nix
+++ modules/hyprland/bindings.nix
@@ -13,6 +13,12 @@ in {
 
         "$mod, W, killactive"
 
+        # Apps
+        "$mod SHIFT, F, nautilus --new-window"
+        "$mod SHIFT, B, exec, xdg-settings get default-web-browser"
+        "$mod SHIFT, E, emacs"
+        "$mod SHIFT, slash, 1password"
+
         # Control Tiling
         "$mod, P, togglesplit"
         "$mod, T, togglefloating"
@@ -50,8 +56,18 @@ in {
         # Screenshot
         ", Print, exec, watershot directory ~/Pictures/Screenshots --copy"
 
-        # TODO: Media Keys
-        # TODO: Groups
+        # Media
+        ", XF86MonBrightnessUp, exec, brightnessctl -q s +10%"
+        ", XF86MonBrightnessDown, exec, brightnessctl -q s 10%-"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, pactl set-sink-mute @DEFAULT_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPause, exec, playerctl pause"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86AudioMicMute, exec, pactl set-source-mute @DEFAULT_SOURCE@ toggle"
+
       ] ++ (builtins.concatLists (builtins.genList (i:
         let ws = i + 1;
         in [
