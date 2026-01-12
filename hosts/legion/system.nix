a0b0b5078c53157e3ef56b4d0d79b3f60f528594
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-12 17:43:13 -0300

    start using legion

--- hosts/legion/system.nix
+++ hosts/legion/system.nix
@@ -16,13 +16,13 @@
 
   boot.initrd.luks.devices = {
     cryptroot = {
-      device = "/dev/disk/by-uuid/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+      device = "/dev/disk/by-uuid/82c28330-9254-4b1c-908a-4ddf127a53d0";
       allowDiscards = true;
       bypassWorkqueues = true;
     };
 
     cryptstorage = {
-      device = "/dev/disk/by-uuid/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+      device = "/dev/disk/by-uuid/1d8fc969-553f-4b90-ae84-95c4621ab338";
       allowDiscards = true;
       bypassWorkqueues = true;
     };
