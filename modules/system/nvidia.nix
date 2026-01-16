0a02a962141a5742817ee6f23d43d75b2507b8b2
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-16 16:53:45 -0300

    feat(nvidia): add nvidia-offload helper

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -1,7 +1,15 @@
 { pkgs, lib, config, ... }:
 
 with lib;
-let cfg = config.modules.system.nvidia;
+let
+  cfg = config.modules.system.nvidia;
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
 in {
   options.modules.system.nvidia = { enable = mkEnableOption "nvidia support"; };
 
@@ -9,6 +17,7 @@ in {
     hardware.graphics.enable = true;
 
     services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
+    environment.systemPackages = [ nvidia-offload ];
 
     hardware.nvidia = {
       modesetting.enable = true;
