b26f0485f5b46044fa21afab4fa44b7889e6c559
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-23 11:43:11 -0300

    feat(niri): Update bindings

--- modules/niri/bindings.nix
+++ modules/niri/bindings.nix
@@ -11,15 +11,18 @@ in {
       sh = spawn "sh" "-c";
     in {
       # Application launchers
-      "Mod+Return".action = spawn "ghostty";
-      "Mod+Space".action = spawn "vicinae";
-      "Mod+Shift+B".action = spawn "zen";
-      "Mod+Shift+F".action = spawn "nautilus";
+      "Mod+Return".action = spawn-sh "GTK_IM_MODULE=simple ghostty";
+      "Mod+Space".action = spawn "vicinae toggle";
+
+      "Mod+Shift+B".action = spawn "zen-beta";
+      "Mod+Shift+F".action = spawn "nautilus --new-window";
+      "Mod+Shift+E".action = spawn-sh "$EDITOR";
+      "Mod+Shift+Slash".action = spawn "1password";
 
       # Window management
       "Mod+W".action = close-window;
       "Mod+Ctrl+Alt+Shift+E".action = quit;
-      "Mod+Shift+Slash".action = show-hotkey-overlay;
+      "Mod+Slash".action = show-hotkey-overlay;
 
       # Focus navigation (vim-style)
       "Mod+H".action = focus-column-left;
@@ -27,29 +30,17 @@ in {
       "Mod+J".action = focus-window-down;
       "Mod+K".action = focus-window-up;
 
-      # Arrow key alternatives
-      "Mod+Left".action = focus-column-left;
-      "Mod+Right".action = focus-column-right;
-      "Mod+Down".action = focus-window-down;
-      "Mod+Up".action = focus-window-up;
-
       # Move windows (vim-style)
       "Mod+Shift+H".action = move-column-left;
       "Mod+Shift+L".action = move-column-right;
       "Mod+Shift+J".action = move-window-down;
       "Mod+Shift+K".action = move-window-up;
 
-      # Arrow key alternatives for moving
-      "Mod+Shift+Left".action = move-column-left;
-      "Mod+Shift+Right".action = move-column-right;
-      "Mod+Shift+Down".action = move-window-down;
-      "Mod+Shift+Up".action = move-window-up;
-
       # First/last column
-      # "Mod+Home".action = focus-column-first;
-      # "Mod+End".action = focus-column-last;
-      # "Mod+Shift+Home".action = move-column-to-first;
-      # "Mod+Shift+End".action = move-column-to-last;
+      "Mod+Alt+H".action = focus-column-first;
+      "Mod+Alt+L".action = focus-column-last;
+      "Mod+Alt+Shift+H".action = move-column-to-first;
+      "Mod+Alt+Shift+L".action = move-column-to-last;
 
       # Monitor focus
       "Mod+Ctrl+H".action = focus-monitor-left;
@@ -60,46 +51,44 @@ in {
       # Move window to monitor
       "Mod+Ctrl+Shift+H".action = move-column-to-monitor-left;
       "Mod+Ctrl+Shift+L".action = move-column-to-monitor-right;
-      "Mod+Ctrl+Shift+Left".action = move-column-to-monitor-left;
-      "Mod+Ctrl+Shift+Right".action = move-column-to-monitor-right;
 
       # Workspace navigation
-      # "Mod+Page_Down".action = focus-workspace-down;
-      # "Mod+Page_Up".action = focus-workspace-up;
+      "Mod+Alt+J".action = focus-workspace-down;
+      "Mod+Alt+K".action = focus-workspace-up;
 
       # Move window to workspace
-      # "Mod+Shift+Page_Down".action = move-column-to-workspace-down;
-      # "Mod+Shift+Page_Up".action = move-column-to-workspace-up;
+      "Mod+Alt+Shift+J".action = move-column-to-workspace-down;
+      "Mod+Alt+Shift+K".action = move-column-to-workspace-up;
 
       # Move workspace to monitor
-      # "Mod+Ctrl+Page_Down".action = move-workspace-down;
-      # "Mod+Ctrl+Page_Up".action = move-workspace-up;
+      "Mod+Alt+Ctrl+J".action = move-workspace-down;
+      "Mod+Alt+Ctrl+K".action = move-workspace-up;
 
       # Workspace switching (1-9)
-      # "Mod+1".action = focus-workspace 1;
-      # "Mod+2".action = focus-workspace 2;
-      # "Mod+3".action = focus-workspace 3;
-      # "Mod+4".action = focus-workspace 4;
-      # "Mod+5".action = focus-workspace 5;
-      # "Mod+6".action = focus-workspace 6;
-      # "Mod+7".action = focus-workspace 7;
-      # "Mod+8".action = focus-workspace 8;
-      # "Mod+9".action = focus-workspace 9;
+      "Mod+1".action = focus-workspace 1;
+      "Mod+2".action = focus-workspace 2;
+      "Mod+3".action = focus-workspace 3;
+      "Mod+4".action = focus-workspace 4;
+      "Mod+5".action = focus-workspace 5;
+      "Mod+6".action = focus-workspace 6;
+      "Mod+7".action = focus-workspace 7;
+      "Mod+8".action = focus-workspace 8;
+      "Mod+9".action = focus-workspace 9;
 
       # Move window to workspace
-      # "Mod+Shift+1".action = move-column-to-workspace 1;
-      # "Mod+Shift+2".action = move-column-to-workspace 2;
-      # "Mod+Shift+3".action = move-column-to-workspace 3;
-      # "Mod+Shift+4".action = move-column-to-workspace 4;
-      # "Mod+Shift+5".action = move-column-to-workspace 5;
-      # "Mod+Shift+6".action = move-column-to-workspace 6;
-      # "Mod+Shift+7".action = move-column-to-workspace 7;
-      # "Mod+Shift+8".action = move-column-to-workspace 8;
-      # "Mod+Shift+9".action = move-column-to-workspace 9;
+      "Mod+Shift+1".action = move-column-to-workspace 1;
+      "Mod+Shift+2".action = move-column-to-workspace 2;
+      "Mod+Shift+3".action = move-column-to-workspace 3;
+      "Mod+Shift+4".action = move-column-to-workspace 4;
+      "Mod+Shift+5".action = move-column-to-workspace 5;
+      "Mod+Shift+6".action = move-column-to-workspace 6;
+      "Mod+Shift+7".action = move-column-to-workspace 7;
+      "Mod+Shift+8".action = move-column-to-workspace 8;
+      "Mod+Shift+9".action = move-column-to-workspace 9;
 
       # Column/window sizing
       "Mod+R".action = switch-preset-column-width;
-      "Mod+Shift+R".action = switch-preset-window-height;
+      "Mod+Alt+R".action = switch-preset-window-height;
       "Mod+F".action = maximize-column;
       "Mod+Alt+F".action = fullscreen-window;
 
@@ -117,7 +106,7 @@ in {
       "Mod+V".action = toggle-window-floating;
       "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
 
-      # Screenshots (niri built-in)
+      # Screenshots
       "Print".action = screenshot;
       "Ctrl+Print".action = screenshot-screen;
       "Alt+Print".action = screenshot-window;
