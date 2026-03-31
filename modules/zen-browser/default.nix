0fdfe7c84c827a9031f35e476490171972b12959
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-03-31 12:34:18 -0300

    feat(zen-browser): add stream space

--- modules/zen-browser/default.nix
+++ modules/zen-browser/default.nix
@@ -5,70 +5,108 @@
   inputs,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.modules.zen-browser;
-in {
-  options.modules.zen-browser = {enable = mkEnableOption "zen-browser";};
+in
+{
+  options.modules.zen-browser = {
+    enable = mkEnableOption "zen-browser";
+  };
 
-  imports = [inputs.zen-browser.homeModules.beta];
+  imports = [ inputs.zen-browser.homeModules.beta ];
 
   config = mkIf cfg.enable {
     programs.zen-browser = {
       enable = true;
 
-      policies = let
-        mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
-          installation_mode = "force_installed";
-        });
-      in {
-        AutofillAddressEnabled = true;
-        AutofillCreditCardEnabled = false;
-        DisableAppUpdate = true;
-        DisableFeedbackCommands = true;
-        DisableFirefoxStudies = true;
-        DisablePocket = true;
-        DisableTelemetry = true;
-        DontCheckDefaultBrowser = true;
-        NoDefaultBookmarks = true;
-        OfferToSaveLogins = false;
-        EnableTrackingProtection = {
-          Value = true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-        };
+      policies =
+        let
+          mkExtensionSettings = builtins.mapAttrs (
+            _: pluginId: {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+              installation_mode = "force_installed";
+            }
+          );
+        in
+        {
+          AutofillAddressEnabled = true;
+          AutofillCreditCardEnabled = false;
+          DisableAppUpdate = true;
+          DisableFeedbackCommands = true;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          DontCheckDefaultBrowser = true;
+          NoDefaultBookmarks = true;
+          OfferToSaveLogins = false;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
 
-        ExtensionSettings = mkExtensionSettings {
-          "languagetool-webextension@languagetool.org" = "languagetool";
-          "enhancerforyoutube@maximerf.addons.mozilla.org" = "enhancer-for-youtube";
-          "myallychou@gmail.com" = "youtube-recommended-videos";
+          ExtensionSettings = mkExtensionSettings {
+            "languagetool-webextension@languagetool.org" = "languagetool";
+            "enhancerforyoutube@maximerf.addons.mozilla.org" = "enhancer-for-youtube";
+            "myallychou@gmail.com" = "youtube-recommended-videos";
+          };
         };
-      };
 
       profiles.managed = {
         id = 0;
         isDefault = true;
         name = "Managed by Nix";
 
-        spacesForce = true;
-        spaces = {
-          "Personal" = {
-            id = "4d929899-3c7c-44e3-be00-e1e850836b6f";
-            icon = "🏠";
-            position = 1000;
+        containersForce = true;
+        containers = {
+          Personal = {
+            color = "purple";
+            icon = "fingerprint";
+            id = 1;
           };
-          "Work" = {
-            id = "1aa8cdd7-cf7b-4523-a2aa-20d3f085dfd3";
-            icon = "💻";
-            position = 2000;
+          Work = {
+            color = "blue";
+            icon = "briefcase";
+            id = 2;
           };
-          "Study" = {
-            id = "cdd10fab-4fc5-494b-9041-325e5759195b";
-            icon = "📕";
-            position = 3000;
+          Stream = {
+            color = "red";
+            icon = "fence";
+            id = 3;
           };
         };
+
+        spacesForce = true;
+        spaces =
+          let
+            containers = config.programs.zen-browser.profiles.managed.containers;
+          in
+          {
+            "Personal" = {
+              id = "4d929899-3c7c-44e3-be00-e1e850836b6f";
+              icon = "🏠";
+              position = 1000;
+            };
+            "Work" = {
+              id = "1aa8cdd7-cf7b-4523-a2aa-20d3f085dfd3";
+              icon = "💻";
+              position = 2000;
+              container = containers."Work".id;
+            };
+            "Study" = {
+              id = "cdd10fab-4fc5-494b-9041-325e5759195b";
+              icon = "📕";
+              position = 3000;
+            };
+            "Stream" = {
+              id = "e38e497a-05b6-4b86-aaae-feb60161464a";
+              icon = "📺";
+              position = 4000;
+              container = containers."Stream".id;
+            };
+          };
 
         pinsForce = true;
         pins = {
@@ -102,7 +140,7 @@ in {
                 ];
               }
             ];
-            definedAliases = ["@s"];
+            definedAliases = [ "@s" ];
           };
         };
 
