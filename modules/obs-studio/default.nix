b6713db52664d3b4eae0c637a14bb8f000dbe895
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-03-31 12:30:16 -0300

    fix(obs): solve multiple crashes and missing encoders

--- modules/obs-studio/default.nix
+++ modules/obs-studio/default.nix
@@ -4,20 +4,31 @@
   config,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.modules.obs-studio;
-in {
-  options.modules.obs-studio = {enable = mkEnableOption "Obs Studio";};
+  nvidia-lib-flag = ''
+    --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
+  '';
+in
+{
+  options.modules.obs-studio = {
+    enable = mkEnableOption "Obs Studio";
+  };
 
   config = mkIf cfg.enable {
     programs.obs-studio = {
       enable = true;
+      package = pkgs.obs-studio.overrideAttrs (old: {
+        preFixup = (old.preFixup or "") + ''
+          wrapProgram $out/bin/obs \
+            ${nvidia-lib-flag}
+        '';
+      });
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs
         obs-backgroundremoval
         obs-pipewire-audio-capture
-        obs-gstreamer
-        obs-vkcapture
       ];
     };
   };
